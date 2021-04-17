9e3414ea5c250f577d5beea6a102dbe2714da3c1
Julien Duchesne <julien.duchesne@grafana.com>
2021-04-17 15:06:00 -0400

    Solve 2019/2

--- advent_of_code_2019/day02.go
+++ advent_of_code_2019/day02.go
@@ -1,7 +1,58 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func tryNounVerb(codeValues []int, noun, verb int) int {
+	code := make([]int, len(codeValues))
+	copy(code, codeValues)
+	position := 0
+	code[1] = noun
+	code[2] = verb
+
+outer:
+	for {
+		switch v := code[position]; v {
+		case 1, 2:
+			first, second, third := code[position+1], code[position+2], code[position+3]
+			if v == 1 {
+				code[third] = code[first] + code[second]
+			} else if v == 2 {
+				code[third] = code[first] * code[second]
+			}
+		case 99:
+			break outer
+		}
+		position = (position + 4) % len(code)
+	}
+	return code[0]
+}
+
 func solveDay02(input string) (string, error) {
-	return "hello" + input, nil
+	var code []int
+	for _, v := range strings.Split(input, ",") {
+		vInt, err := strconv.Atoi(v)
+		if err != nil {
+			return "", err
+		}
+		code = append(code, vInt)
+	}
+
+	part2 := 0
+outer:
+	for i := 1; i <= 20; i++ {
+		for j := 1; j <= 20; j++ {
+			if tryNounVerb(code[:], i, j) == 19690720 {
+				part2 = i*100 + j
+				break outer
+			}
+		}
+	}
+
+	return fmt.Sprintf("Part 1: %d\nPart 2: %d", tryNounVerb(code[:], 12, 2), part2), nil
 }
 
 func init() {
